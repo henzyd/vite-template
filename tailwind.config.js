649d495ba5a660154e6fbd2cfed434ef9cf38f43
henzyd <henzydee@gmail.com>
2023-11-20 15:13:04 +0100

    added screen size to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        smallMobile: { max: '320px' },
+        // => @media (max-width: 320px) { ... }
+
+        mediumMobile: { max: '375px' },
+        // => @media (max-width: 375px) { ... }
+
+        largeMobile: { max: '425px' },
+        // => @media (max-width: 425px) { ... }
+
+        tablet: { max: '768px' },
+        // => @media (max-width: 768px) { ... }
+
+        tabletAndBelow: { max: '1024px' },
+        // => @media (max-width:1024px) { ... }
+
+        laptopAndAbove: { min: '1024px' },
+        // => @media (min-width:1024px) { ... }
+
+        largeLaptop: { min: '1440px' },
+        // => @media (min-width: 1440px) { ... }
+
+        '4kDesktop': { min: '2560px' },
+        // => @media (min-width: 2560px) { ... }
+      },
+    },
   },
   plugins: [],
 }
