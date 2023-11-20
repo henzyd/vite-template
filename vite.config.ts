e3a9777a951ca72641834cd47388ff8932f49a66
henzyd <henzydee@gmail.com>
2023-11-20 15:59:49 +0100

    updated external files

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-import path from 'path'
+import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "src"),
+      },
     },
-  },
-  plugins: [react()],
-})
+    server: {
+      port: parseInt(process.env.VITE_PORT) || 5173,
+      host: true,
+    },
+  });
+};
