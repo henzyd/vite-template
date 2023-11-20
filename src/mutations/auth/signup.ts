f935e7a199039bb57096197ede381bfbb5a7d7db
henzyd <henzydee@gmail.com>
2023-11-20 17:08:50 +0100

    updated signup mutation file

--- src/mutations/auth/signup.ts
+++ src/mutations/auth/signup.ts
@@ -1,12 +1,39 @@
+import axios, { isAxiosError } from "axios";
 import { useMutation } from "@tanstack/react-query";
-import axios from "axios";
+import { notifyError, notifySuccess } from "~/utils/toast";
 
-async function signUp(data: Record<string, any>) {
-  const { data: response } = await axios.post("auth/users/", data);
+function useSignUp() {
+  return useMutation({
+    mutationFn: async (
+      data: Record<"first_name" | "last_name" | "email" | "password" | "re_password", string>
+    ) => {
+      const { data: response } = await axios.post("/auth/users/", { ...data, role: "user" });
 
-  return response.data;
+      return response;
+    },
+    onSuccess: () => {
+      notifySuccess("Account created successfully!");
+    },
+    onError: (error) => {
+      if (isAxiosError(error)) {
+        if (error.response?.status === 400) {
+          if (error.response.data.password?.[0].includes("This password is too common.")) {
+            notifyError("This password is too common, please try another one");
+          } else if (error.response.data.email?.[0]) {
+            notifyError("User with this email already exists");
+          } else {
+            notifyError("Invalid email or password, please check your inputs and try again");
+          }
+        } else if (error.response?.status === 401) {
+          notifyError(error.response.data.error);
+        } else {
+          if (typeof error === "string") {
+            notifyError(error);
+          }
+        }
+      }
+    },
+  });
 }
 
-export default function useSignUp() {
-  return useMutation(signUp);
-}
+export default useSignUp;
