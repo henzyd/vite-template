766ead9db04d71168e2db60467abf3df54cb967f
henzyd <henzydee@gmail.com>
2024-01-17 00:56:05 +0100

    refactor: added a customizable props to label

--- src/components/form-field.tsx
+++ src/components/form-field.tsx
@@ -1,17 +1,24 @@
-import { InputLabel, OutlinedInputProps, OutlinedInput } from "@mui/material";
+import { InputLabel, InputLabelProps, OutlinedInputProps, OutlinedInput } from "@mui/material";
 import { ErrorMessage, Field, FieldConfig, FieldProps } from "formik";
 import { twMerge } from "tailwind-merge";
 
-type FormFieldProps = OutlinedInputProps & FieldConfig;
+type FormFieldProps = OutlinedInputProps &
+  FieldConfig & {
+    labelProps?: InputLabelProps;
+  };
 
-export default function FormField({ label, className, ...props }: FormFieldProps) {
+export default function FormField({ label, className, labelProps, ...props }: FormFieldProps) {
   return (
     <Field {...props}>
       {({ field }: FieldProps) => (
         <div className="w-full">
           {label && (
             <div className="w-full flex justify-between items-center">
-              <InputLabel htmlFor={props.name} className="!text-sm mb-[0.35rem]">
+              <InputLabel
+                {...labelProps}
+                htmlFor={props.name}
+                className={twMerge(`!text-sm mb-[0.35rem] !font-Work-Sans`, labelProps?.className)}
+              >
                 {label}
                 {props.required && <span className="!text-red-600 !text-[0.9rem] pl-1">*</span>}
               </InputLabel>
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,5 +1,17 @@
 import { createTheme } from "@mui/material";
 
-const theme = createTheme({});
+const theme = createTheme({
+  components: {
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          "& .MuiOutlinedInput-input": {
+            padding: "11px 13.5px",
+          },
+        },
+      },
+    },
+  },
+});
 
 export default theme;
