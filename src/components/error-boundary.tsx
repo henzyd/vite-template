a2e08dad8d294a31451a4c9e67b4162043cce24e
henzyd <henzydee@gmail.com>
2024-01-17 00:54:55 +0100

    style: styled error-boundary page

--- src/components/error-boundary.tsx
+++ src/components/error-boundary.tsx
@@ -1,5 +1,6 @@
-import { Button } from "@mui/material";
 import { useRouteError } from "react-router-dom";
+import Button from "~/components/button";
+import ErrorOccured from "~/assets/illustrations/error-occured.png";
 
 export default function ErrorBoundary() {
   const error = useRouteError();
@@ -8,10 +9,17 @@ export default function ErrorBoundary() {
 
   return (
     <>
-      <h1>An error occurred!</h1>
-      <Button variant="contained" color="primary" onClick={() => window.location.reload()}>
-        Reload Page
-      </Button>
+      <div className="w-full min-h-screen flex flex-col items-center justify-center gap-2 p-8">
+        <figure className="w-[28%] largeMobile:w-[55%] mediumMobile:!w-[70%]">
+          <img src={ErrorOccured} alt="Error Boundary Illustration" />
+        </figure>
+        <p className="mt-6 mb-2 text-center mediumMobile:text-sm">
+          An error occured. Please reload the page or try again later.
+        </p>
+        <div className="flex items-center mt-4 justify-center">
+          <Button onClick={() => window.location.reload()}>Reload Page</Button>
+        </div>
+      </div>
     </>
   );
 }
